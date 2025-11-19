69022d19bebcbe0f089786969b6cfbdfb5b4cc25
Shakil Mahmud <shakil.regnum@gmail.com>
2025-11-19 07:31:21 +0000

    change the currency $ to ৳

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,6 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 export function formatCurrency(amount: number) {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
-    currency: "USD",
+    currency: "BDT",
+    currencyDisplay: "symbol",
   }).format(amount);
 }
